a4e04f6edf871c43ccee5bd79d2bfa119e272531
Christopher Pappas <cpappas@edx.org>
2018-10-12 17:26:33 -0400

    Setting up the basic structure for enterprise_reporting test coverage

---
+++ enterprise_reporting/tests/test_export_edxapp_data.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Test export edxapp data.
+"""
+
+import unittest
+
+from enterprise_reporting import export_edxapp_data
+
+
+class TestUtilities(unittest.TestCase):
+	pass
---
+++ enterprise_reporting/tests/test_reporter.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Test reporter.
+"""
+
+import unittest
+
+from enterprise_reporting import reporter
+
+
+class TestUtilities(unittest.TestCase):
+	pass
