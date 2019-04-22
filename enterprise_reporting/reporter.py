076b952e3377da5c74e70544e75d5c97e0a073f3
Chris Pappas <christopappas@users.noreply.github.com>
2019-04-22 15:59:47 -0400

    Fixing format strings (#125)

--- enterprise_reporting/reporter.py
+++ enterprise_reporting/reporter.py
@@ -149,9 +149,9 @@ class EnterpriseReportSender(object):
     def _generate_enterprise_report_progress_v2_csv(self):
         """Query the Enterprise Data API to get progress data to be turned into a CSV."""
         client = EnterpriseDataApiClient()
-        LOGGER.info("dir() for the enterprise data client: ".format(dir(client)))
-        LOGGER.info("client for the enterprise data client:".format(client.client))
-        LOGGER.info("client_id for the enterprise data client".format(client.client_id))
+        LOGGER.info("dir() for the enterprise data client: {}".format(dir(client)))
+        LOGGER.info("client for the enterprise data client: {}".format(client.client))
+        LOGGER.info("client_id for the enterprise data client: {}".format(client.client_id))
         enrollments = client.get_enterprise_enrollments(self.enterprise_customer_uuid)['results']
         if not enrollments:
             return []
@@ -167,9 +167,9 @@ class EnterpriseReportSender(object):
         Query the Enterprise Data API to get progress data to be turned into json.
         """
         client = EnterpriseDataApiClient()
-        LOGGER.info("dir() for the enterprise data client: ".format(dir(client)))
-        LOGGER.info("client for the enterprise data client:".format(client.client))
-        LOGGER.info("client_id for the enterprise data client".format(client.client_id))
+        LOGGER.info("dir() for the enterprise data client: {}".format(dir(client)))
+        LOGGER.info("client for the enterprise data client: {}".format(client.client))
+        LOGGER.info("client_id for the enterprise data client: {}".format(client.client_id))
         enrollments = client.get_enterprise_enrollments(self.enterprise_customer_uuid)['results']
         report_data = enrollments if enrollments else []
 
