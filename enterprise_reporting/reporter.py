91ea98f8f1da45e70f459a78ece3656477a9f862
Christopher Pappas <cpappas@edx.org>
2019-04-23 11:20:14 -0400

    Removing some logging i had added for troubleshooting purposes

--- enterprise_reporting/reporter.py
+++ enterprise_reporting/reporter.py
@@ -148,11 +148,7 @@ class EnterpriseReportSender(object):
 
     def _generate_enterprise_report_progress_v2_csv(self):
         """Query the Enterprise Data API to get progress data to be turned into a CSV."""
-        client = EnterpriseDataApiClient()
-        LOGGER.info("dir() for the enterprise data client: {}".format(dir(client)))
-        LOGGER.info("client for the enterprise data client: {}".format(client.client))
-        LOGGER.info("client_id for the enterprise data client: {}".format(client.client_id))
-        enrollments = client.get_enterprise_enrollments(self.enterprise_customer_uuid)['results']
+        enrollments = EnterpriseDataApiClient().get_enterprise_enrollments(self.enterprise_customer_uuid)['results']
         if not enrollments:
             return []
         with open(self.data_report_file_name, 'w') as data_report_file:
@@ -166,11 +162,7 @@ class EnterpriseReportSender(object):
         """
         Query the Enterprise Data API to get progress data to be turned into json.
         """
-        client = EnterpriseDataApiClient()
-        LOGGER.info("dir() for the enterprise data client: {}".format(dir(client)))
-        LOGGER.info("client for the enterprise data client: {}".format(client.client))
-        LOGGER.info("client_id for the enterprise data client: {}".format(client.client_id))
-        enrollments = client.get_enterprise_enrollments(self.enterprise_customer_uuid)['results']
+        enrollments = EnterpriseDataApiClient().get_enterprise_enrollments(self.enterprise_customer_uuid)['results']
         report_data = enrollments if enrollments else []
 
         with open(self.data_report_file_name, 'w') as data_report_file:
