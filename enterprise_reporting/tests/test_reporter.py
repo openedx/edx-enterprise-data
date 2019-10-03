54d60e37d028c0b3a027da307f1f930c7e495f99
Sameen Fatima <sameen@unassigned.arbisoft.com>
2019-10-03 12:53:27 +0500

    added-comments-and-improvements

--- enterprise_reporting/tests/test_reporter.py
+++ enterprise_reporting/tests/test_reporter.py
@@ -12,54 +12,64 @@ from enterprise_reporting.reporter import EnterpriseReportSender
 from enterprise_reporting.utils import encrypt_string
 
 
-@pytest.mark.skip(six.PY2, "Not compatible with Python 2")
+# @pytest.mark.skip(six.PY2, "Not compatible with Python 2")
 class TestReporter(unittest.TestCase):
-
+	""""
+	Tests about reporter methods
+	"""
 	def setUp(self):
 		self.FILE_WRITE_DIRECTORY = '/tmp'
 		self.date = datetime.datetime.now().strftime("%Y-%m-%d")
+		self.reporting_config = {
+			'enterprise_customer': {
+				'uuid': '4815162242', 'name': 'John'
+			},
+			'delivery_method': 'sftp',
+			'data_type': 'catalog',
+			'uuid': '987654321',
+			'sftp_hostname': '127.0.0.1',
+			'active': True,
+			'report_type': 'json',
+			'encrypted_password': 'bleeH-bLah-blUe001',
+			'sftp_username': 'John1',
+			'sftp_port': 22,
+			'sftp_file_path': 'home/user/Documents',
+			'encrypted_sftp_password': 'abraAcaDabradOo',
+		}
 
 	def test_data_report_file_name_with_date(self):
-		reporting_config = {
-			'enterprise_customer': {
-				'uuid': '12345678910', 'name': 'John'
-			},
-			'delivery_method': 'sftp', 'data_type': 'catalog', 'uuid': '987654321', 'sftp_hostname': '127.0.0.1', 'active': True,
-			'report_type': 'json', 'encrypted_password': 'bleeH-bLah-blUe001', 'sftp_username': 'John1', 'sftp_port': 22,
-			'sftp_file_path': 'home/user/Documents', 'encrypted_sftp_password': 'abraAcaDabradOo', 'include_date': True
-		}
-		sftp_password = reporting_config['encrypted_sftp_password']
-		reporting_config['encrypted_sftp_password'] = encrypt_string(sftp_password)
-		enterprise_reporter = EnterpriseReportSender.create(reporting_config)
+		"""
+		Tests if the report config file name is generated correctly when date is included
+		"""
+		self.reporting_config['include_date'] = True
+		sftp_password = self.reporting_config['encrypted_sftp_password']
+		self.reporting_config['encrypted_sftp_password'] = encrypt_string(sftp_password)
+		enterprise_reporter = EnterpriseReportSender.create(self.reporting_config)
 		actual_file_name = enterprise_reporter.data_report_file_name
-		expected_name = "{dir}/{enterprise_uuid}_{data_type}_{report_type}_{date}.{report_type}".format(
+		expected_file_name = "{dir}/{enterprise_uuid}_{data_type}_{report_type}_{date}.{report_type}".format(
 			dir=self.FILE_WRITE_DIRECTORY,
-			enterprise_uuid=reporting_config['enterprise_customer']['uuid'],
-			data_type=reporting_config['data_type'],
+			enterprise_uuid=self.reporting_config['enterprise_customer']['uuid'],
+			data_type=self.reporting_config['data_type'],
 			date=self.date,
-			report_type=reporting_config['report_type']
+			report_type=self.reporting_config['report_type']
 			)
-		assert actual_file_name == expected_name
+		assert actual_file_name == expected_file_name
 
 	def test_data_report_file_name_without_date(self):
-		reporting_config = {
-			'enterprise_customer': {
-				'uuid': '4815162342', 'name': 'Amy'
-			},
-			'delivery_method': 'sftp', 'data_type': 'catalog', 'uuid': '42135342-12345', 'sftp_hostname': '127.0.0.1', 'active': True,
-			'report_type': 'csv', 'encrypted_password': 'bleeH-bLah-blUe001', 'sftp_username': 'Amy22', 'sftp_port': 80,
-			'sftp_file_path': 'home/user/Documents', 'encrypted_sftp_password': 'abraAcaDabradOo',
-			'include_date': False
-		}
-		sftp_password = reporting_config['encrypted_sftp_password']
-		reporting_config['encrypted_sftp_password'] = encrypt_string(sftp_password)
-		enterprise_reporter = EnterpriseReportSender.create(reporting_config)
+		"""
+		Tests if the report config file name is generated correctly when date is not included
+		"""
+		import pdb; pdb.set_trace()
+		self.reporting_config['include_date'] = False
+		sftp_password = self.reporting_config['encrypted_sftp_password']
+		self.reporting_config['encrypted_sftp_password'] = encrypt_string(sftp_password)
+		enterprise_reporter = EnterpriseReportSender.create(self.reporting_config)
 		actual_file_name = enterprise_reporter.data_report_file_name
-		expected_name = "{dir}/{enterprise_uuid}_{data_type}_{report_type}.{report_type}".format(
+		expected_file_name = "{dir}/{enterprise_uuid}_{data_type}_{report_type}.{report_type}".format(
 			dir=self.FILE_WRITE_DIRECTORY,
-			enterprise_uuid=reporting_config['enterprise_customer']['uuid'],
-			data_type=reporting_config['data_type'],
+			enterprise_uuid=self.reporting_config['enterprise_customer']['uuid'],
+			data_type=self.reporting_config['data_type'],
 			date=self.date,
-			report_type=reporting_config['report_type']
+			report_type=self.reporting_config['report_type']
 		)
-		assert actual_file_name == expected_name
+		assert actual_file_name == expected_file_name
