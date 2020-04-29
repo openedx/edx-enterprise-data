93c6840bb8514537d8d2a2611184c57f4949b110
Luis Moreno <luis.moreno@edunext.co>
2020-04-29 17:21:58 -0400

    Upgrade packages

--- setup.py
+++ setup.py
@@ -58,9 +58,6 @@ setup(
     version=VERSION,
     classifiers=[
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
