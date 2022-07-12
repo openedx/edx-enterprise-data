9b5b076afd07843c90c4438596aceffc659568eb
Adam Stankiewicz <agstanki@gmail.com>
2022-07-12 12:01:47 -0400

    build: add temporary print for debugging pt4

--- enterprise_reporting/clients/enterprise.py
+++ enterprise_reporting/clients/enterprise.py
@@ -128,8 +128,8 @@ class EnterpriseCatalogAPIClient(EdxOAuth2APIClient):
                 should_traverse_pagination=True,
                 querystring={'page_size': self.PAGE_SIZE},
             )
-            LOGGER.info('get_content_metadata', dir(traversed_metadata), type(traversed_metadata), traversed_metadata)
-            transformed_metadata = self.transform_get_content_metadata(traversed_metadata)
+            LOGGER.info('get_content_metadata', dir(traversed_metadata), type(traversed_metadata))
+            transformed_metadata = self.transform_get_content_metadata(traversed_metadata.get('results'))
             content_metadata.update(transformed_metadata)
 
         # We only made this a dictionary to help filter out duplicates by a common key. We just want values now.
