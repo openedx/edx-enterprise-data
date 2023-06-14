6f29ca3c55c11fe72d5a7627fce8cfdc53047b1c
Brian Beggs <macdiesel@gmail.com>
2023-06-14 18:35:36 -0400

    chore: add test cases for ecommerce offer_id types

--- enterprise_data/tests/api/v1/test_serializers.py
+++ enterprise_data/tests/api/v1/test_serializers.py
@@ -52,6 +52,7 @@ class TestEnterpriseOfferSerializer(APITransactionTestCase):
     @ddt.data(
         ('787f0fa7-f89a-4267-9e2b-fcd299e83918', '787f0fa7f89a42679e2bfcd299e83918'),
         ('56a779a5-21ec-4711-830f-6654d3faaa5f', '56a779a521ec4711830f6654d3faaa5f'),
+        ('12345', 12345),
     )
     @ddt.unpack
     def test_deserialize_offer_id_valid(self, offer_id_request, offer_id_db):
@@ -67,7 +68,8 @@ class TestEnterpriseOfferSerializer(APITransactionTestCase):
         # Representations that could be real UUIDs should always be normalized to un-hyphenated UUIDs.
         ('787f0fa7f89a42679e2bfcd299e83918', '787f0fa7-f89a-4267-9e2b-fcd299e83918'),
         ('f0d5fcdbed8e4d408f3b79d1671f967f', 'f0d5fcdb-ed8e-4d40-8f3b-79d1671f967f'),
-        ('c1d39f8a3fd246758bf7f56dd076694d', 'c1d39f8a-3fd2-4675-8bf7-f56dd076694d')
+        ('c1d39f8a3fd246758bf7f56dd076694d', 'c1d39f8a-3fd2-4675-8bf7-f56dd076694d'),
+        ('12345', '12345'),
     )
     @ddt.unpack
     def test_serialize_offer_id_valid(self, offer_id_db, offer_id_serialized):
