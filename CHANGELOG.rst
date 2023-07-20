dafb46b9de3a846204c41f207a1d980bf1197fcf
salmannawaz <salman.nawaz@arbisoft.com>
2023-07-20 13:22:10 +0500

    Django 4.2 upgrade (#375)
    * fix: django 4.2 upgrade
    
    ---------
    
    Co-authored-by: Salman Nawaz <salman.nawaz@A006-01081.local>
    Co-authored-by: UsamaSadiq <usama7274@gmail.com>

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -15,6 +15,11 @@ Unreleased
 ----------
 
 =========================
+[4.9.0] - 2023-07-20
+---------------------
+  * Support added for Django 4.2
+
+
 [4.8.1] - 2023-07-14
 ---------------------
   *  Sort enterprise enrollments by default on last_activity_date.
--- enterprise_data/__init__.py
+++ enterprise_data/__init__.py
@@ -2,7 +2,6 @@
 Enterprise data api application. This Django app exposes API endpoints used by enterprises.
 """
 
-__version__ = "4.8.1"
-
+__version__ = "4.9.0"
 
 default_app_config = "enterprise_data.apps.EnterpriseDataAppConfig"  # pylint: disable=invalid-name
