e9d59370968fcd2242844e2be2f7d40bf2d94cf6
Usama Sadiq <usama.sadiq@arbisoft.com>
2024-10-25 16:36:02 +0500

    feat!: Python 3.12 Upgrade

--- setup.py
+++ setup.py
@@ -133,7 +133,6 @@ setup(
         'Framework :: Django :: 4.2',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.12',
     ],
     description="""Enterprise Reporting""",
     long_description="Tools and products related to providing access to Enterprise data.",
