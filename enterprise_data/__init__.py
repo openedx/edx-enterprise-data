dda2e218539726cb63c4ac7f1362687ef6f47f92
Usama Sadiq <usama.sadiq@A006-01413.local>
2024-10-31 12:44:42 +0500

    build: bump version to 9.7.1

--- enterprise_data/__init__.py
+++ enterprise_data/__init__.py
@@ -2,8 +2,4 @@
 Enterprise data api application. This Django app exposes API endpoints used by enterprises.
 """
 
-<<<<<<< HEAD
-__version__ = "9.7.0"
-=======
-__version__ = "10.1.0"
->>>>>>> 2dbca4e (feat: Added management command to pre-warm analytics data.)
+__version__ = "9.7.1"
